dbf98efe437fe72e54a1e31234f0ed85d978e939
Leo Yuan <44822801+TheBoss77@users.noreply.github.com>
2024-01-18 17:11:16 -0500

    Add files via upload

---
+++ Climbing_Stairs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int climbStairs(int n){
+    int dp[n+1];
+    dp[1] = 1;
+    dp[0] = 1;
+    for(int i = 2; i <= n; i++){
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+int main()
+{
+    cout<<climbStairs(3)<<"\n";
+    return 0;
+}
